71984c80b238ca0e855a91ba5f7927eaf87a6d2c
Gabriel Mioni <104784563+gab3mioni@users.noreply.github.com>
2025-03-12 09:56:17 -0300

    refactor: criando containerBuilder com injeção de dependência

--- app/config/bootstrap.php
+++ app/config/bootstrap.php
@@ -1,29 +1,52 @@
 <?php
 
+declare(strict_types=1);
+
+use DI\Container;
+use DI\ContainerBuilder;
+use Gab3mioni\ApiLlm\Controllers\ValidationController;
+use Gab3mioni\ApiLlm\Interface\ValidationInterface;
+use Gab3mioni\ApiLlm\Services\ValidationService;
+use GuzzleHttp\Client;
+use Slim\Factory\AppFactory;
+use Symfony\Component\Dotenv\Dotenv;
+
 set_time_limit(240);
 
 require __DIR__ . '/../../vendor/autoload.php';
 
-use Gab3mioni\ApiLlm\Controllers\ValidationController;
-use Gab3mioni\ApiLlm\Services\ValidationService;
-use GuzzleHttp\Client;
-use Symfony\Component\Dotenv\Dotenv;
-use Slim\Factory\AppFactory;
-
 $dotenv = new Dotenv();
 $dotenv->loadEnv(__DIR__ . '/../../.env');
 
-$app = AppFactory::create();
+$containerBuilder = new ContainerBuilder();
+$containerBuilder->addDefinitions([
+    Client::class => function () {
+        return new Client([
+            'verify' => false,
+            'timeout' => 120
+        ]);
+    },
+    ValidationInterface::class => function (Container $c) {
+        return new ValidationService(
+            $c->get(Client::class),
+            $_ENV['DEEPSEEK_ENDPOINT'],
+            $_ENV['DEEPSEEK_MODEL']
+        );
+    },
+    ValidationController::class => function (Container $c) {
+        return new ValidationController(
+            $c->get(ValidationInterface::class)
+        );
+    }
+]);
+
+$container = $containerBuilder->build();
+
+$app = AppFactory::createFromContainer($container);
+
 $app->addBodyParsingMiddleware();
 $app->addErrorMiddleware(true, true, true);
 
-$client = new Client(['verify' => false, 'timeout' => 120]);
-$endpoint = $_ENV['DEEPSEEK_ENDPOINT'];
-$model = $_ENV['DEEPSEEK_MODEL'];
-
-$validatorService = new ValidationService($client, $endpoint, $model);
-$controller = new ValidationController($validatorService);
-
-$app->post('/validar-mensagem', [$controller, 'validate']);
+$app->post('/validar-mensagem', ValidationController::class . ':validate');
 
 return $app;
